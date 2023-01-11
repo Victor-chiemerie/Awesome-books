6628f2ec439b2e3eab0e1fd6e05f1e8d298bbc19
Victor-chiemerie <victorchiemerie02@gmail.com>
2023-01-11 22:42:31 +0100

    Modify webpage with navigations

--- main.js
+++ main.js
@@ -25,12 +25,12 @@ class Library {
   }
 }
 
-const booklist = document.querySelector('#booklist');
-const inputTitle = document.getElementById('title');
-const inputAuthor = document.getElementById('author');
-const buttonAdd = document.getElementById('add_button');
+const booklist = document.querySelector("#booklist");
+const inputTitle = document.getElementById("title");
+const inputAuthor = document.getElementById("author");
+const buttonAdd = document.getElementById("add_button");
 
-const stringShelf = localStorage.getItem('shelf');
+const stringShelf = localStorage.getItem("shelf");
 const obj = new Library();
 
 if (stringShelf) {
@@ -41,7 +41,7 @@ if (stringShelf) {
 }
 
 function printBooks() {
-  let innerhtml = '';
+  let innerhtml = "";
 
   shelf.forEach((book, index) => {
     innerhtml += `
@@ -56,22 +56,62 @@ function printBooks() {
 
   shelf.forEach((book, index) => {
     const removeBtn = document.getElementById(`remove-btn${index}`);
-    removeBtn.addEventListener('click', () => {
+    removeBtn.addEventListener("click", () => {
       obj.removeBook(index);
       printBooks();
     });
   });
 
-  localStorage.setItem('shelf', JSON.stringify(shelf));
+  localStorage.setItem("shelf", JSON.stringify(shelf));
 }
 
 printBooks();
 
-buttonAdd.addEventListener('click', (event) => {
+buttonAdd.addEventListener("click", (event) => {
   event.preventDefault();
   obj.addBook(inputTitle.value, inputAuthor.value);
   printBooks();
 
-  inputTitle.value = '';
-  inputAuthor.value = '';
+  inputTitle.value = "";
+  inputAuthor.value = "";
 });
+
+// Web Navigation Section
+
+const list = document.querySelector(".list")
+const add_new = document.querySelector(".add_new")
+const contact = document.querySelector(".contact")
+const listBtn = document.querySelector(".listBtn")
+const add_newBtn = document.querySelector(".add_newBtn")
+const contactBtn = document.querySelector(".contactBtn")
+
+function ShowList(id) {
+  list.classList.remove('hidden');
+  add_new.classList.add('hidden');
+  contact.classList.add('hidden');
+  listBtn.style.color = "rgb(55, 135, 172)";
+  add_newBtn.style.color = "black";
+  contactBtn.style.color = "black";
+}
+
+function ShowAddNew(id) {
+  list.classList.add('hidden');
+  add_new.classList.remove('hidden');
+  contact.classList.add('hidden');
+  listBtn.style.color = "black";
+  add_newBtn.style.color = "rgb(55, 135, 172)";
+  contactBtn.style.color = "black";
+}
+
+function ShowContact(id) {
+  list.classList.add('hidden');
+  add_new.classList.add('hidden');
+  contact.classList.remove('hidden');
+  listBtn.style.color = "black";
+  add_newBtn.style.color = "black";
+  contactBtn.style.color = "rgb(55, 135, 172)";
+}
+
+function displayDate() {
+  const date = document.querySelector(".date").innerHTML = Date();
+}
